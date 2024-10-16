6017e9247276c1f209d9321e666f7753eabe667c
VietTranCong <anhhong141203@gmail.com>
2024-10-16 17:40:57 +0700

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-// import { Routes, Route, Navigate } from 'react-router-dom';
-// import { PhotoList, PhotoDetail } from './components';
+import { Routes, Route } from 'react-router-dom';
+import { PhotoList, PhotoDetail } from './components';
 
 const App = () => {
   return (
-    // <Routes>
-    //   <Route path="/photos/:id" element={<PhotoDetail />} />
-    //   <Route path="/photos" element={<PhotoList />} />
-    //   <Route path="/" element={<Navigate to="/photos" />} />
-    // </Routes>
-    <div className="text-center">123</div>
+    <Routes>
+      <Route path="/photos/:id" element={<PhotoDetail />} />
+      <Route exact path="/photos" element={<PhotoList />} />
+    </Routes>
   );
 };
 
